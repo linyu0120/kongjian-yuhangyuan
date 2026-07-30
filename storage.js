62b94af215d59a50ce4db7d81195b0e0c943df78
linyu0120 <tiger88120@gmail.com>
2026-07-30 23:43:24 +0800

    storage.js

--- storage.js
+++ storage.js
@@ -1,19 +1,27 @@
-// 🏸 空間羽航員 V5.6 Final
-// 本機資料保存模組
+function saveData(key, value){
 
-function saveData(key, value) {
   localStorage.setItem(
     key,
     JSON.stringify(value)
   );
+
 }
 
-function loadData(key) {
-  let data = localStorage.getItem(key);
 
-  if (data) {
+
+function loadData(key){
+
+  let data =
+  localStorage.getItem(key);
+
+
+  if(data){
+
     return JSON.parse(data);
+
   }
 
+
   return null;
+
 }
