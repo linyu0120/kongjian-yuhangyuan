e7168d9a2ae703a88acc1bf24535305569cc03be
linyu0120 <tiger88120@gmail.com>
2026-07-30 23:37:24 +0800

    app.js

--- app.js
+++ app.js
@@ -1,85 +1,203 @@
 let players = JSON.parse(localStorage.getItem("players")) || [];
 
+let currentRound = 0;
+
+
 
 function savePlayers(){
+
   localStorage.setItem(
     "players",
     JSON.stringify(players)
   );
+
 }
+
 
 
 function addPlayer(){
 
-  let input = document.getElementById("playerName");
-  let name = input.value.trim();
+  let input =
+  document.getElementById("playerName");
+
+
+  let name =
+  input.value.trim();
+
 
   if(name === ""){
-    alert("請輸入姓名");
+
+    alert("請輸入球友姓名");
+
     return;
+
   }
+
 
   players.push(name);
 
+
   savePlayers();
+
 
   input.value="";
 
+
   showPlayers();
+
 }
+
+
 
 
 function removePlayer(index){
 
+
   players.splice(index,1);
+
 
   savePlayers();
 
+
   showPlayers();
 
+
 }
+
+
+
 
 
 function showPlayers(){
 
-  let list=document.getElementById("playerList");
 
-  list.innerHTML="";
+ let list =
+ document.getElementById("playerList");
 
-  players.forEach((p,i)=>{
 
-    let li=document.createElement("li");
+ list.innerHTML="";
 
-    li.innerHTML =
-    p+" <button onclick='removePlayer("+i+")'>❌</button>";
 
-    list.appendChild(li);
+ players.forEach(function(player,index){
 
-  });
+
+   let li =
+   document.createElement("li");
+
+
+   li.innerHTML =
+   player +
+   " <button onclick='removePlayer("+
+   index+
+   ")'>❌</button>";
+
+
+   list.appendChild(li);
+
+
+ });
+
 
 }
 
 
-function start(){
-
- let courts=
- Number(document.getElementById("courtCount").value);
-
- let people=
- Number(document.getElementById("peopleCount").value);
 
 
- let result=createSchedule(
-   players,
-   courts,
-   people
+function startSchedule(){
+
+
+ if(players.length===0){
+
+   alert("請先加入球友");
+
+   return;
+
+ }
+
+
+ currentRound=0;
+
+
+ let courts =
+ Number(
+ document.getElementById("courtCount").value
  );
 
 
- document.getElementById("result").innerHTML=
- result.join("<br>");
+ let people =
+ Number(
+ document.getElementById("peopleCount").value
+ );
+
+
+ let result =
+ createSchedule(
+ players,
+ courts,
+ people,
+ currentRound
+ );
+
+
+ showResult(result);
+
 
 }
+
+
+
+
+
+function nextRound(){
+
+
+ currentRound++;
+
+
+ let courts =
+ Number(
+ document.getElementById("courtCount").value
+ );
+
+
+ let people =
+ Number(
+ document.getElementById("peopleCount").value
+ );
+
+
+ let result =
+ createSchedule(
+ players,
+ courts,
+ people,
+ currentRound
+ );
+
+
+ showResult(result);
+
+
+}
+
+
+
+
+
+function showResult(data){
+
+
+ let box =
+ document.getElementById("result");
+
+
+ box.innerHTML =
+ data.join("<br>");
+
+}
+
+
+
 
 
 showPlayers();
